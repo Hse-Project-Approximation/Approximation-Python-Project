e18d774ed8561f8320ecc06003c35cda88e184f8
Desaje13 <Sergeygetun@gmail.com>
2023-02-01 00:19:59 +0300

    Graph visualisation on website
    - Визуализация графика на веб-сайте
    -Выбор координат из csv файла пользователя с возможностью выбора

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
-from sympy import diff, symbols, cos, sin, Function, Derivative, solve, Matrix, plot
+from flask import Flask, send_file, render_template, request
+import io
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import numpy as np
 import csv
+from numpy import array
 
-#read coordinates
-x_arr, y_arr = np.array(list()), np.array(list())
-with open('coord.csv', 'r') as csv_file:
-  csv_reader = csv.reader(csv_file)
-  for line in csv_reader:
-    t = ''.join(line).split(';')
-    x_arr = np.append(x_arr, t[0])
-    y_arr = np.append(y_arr, t[1])
+app = Flask(__name__)
 
-#show the raw graphics
-#plt.plot(x_arr, y_arr)
-#plt.show()
+fig, ax=plt.subplots(figsize=(10, 10))
+ax=sns.set_style(style="darkgrid")
 
-#creating necessary functions
-c1, c2, x, y = symbols('c1 c2 x y')
-r_func = c1*cos(x) + c2*sin(x)
-i_func = y - r_func
 
-#solving df
-c1_df = diff(r_func, c1)
-print(c1_df)
-c2_df = diff(r_func, c2)
-print(c2_df)
-slu = solve([c1_df, c2_df], c1, c2, dict=True)
-print(slu)
+@app.route('/', methods=['GET', 'POST'])
+def index():
 
-#checking if correct
-det_min2 = Matrix([[diff(c1_df, c1), diff(c1_df, c2)], [diff(c2_df, c1), diff(c2_df, c2)]]).det()
-det_min1 = diff(c1_df, c1)
+    return render_template('index.html')
 
-print(det_min1, det_min2)
+@app.route('/data', methods=['GET', 'POST'])
+def data():
 
-res = 0
-for i in range(len(x_arr)):
-  t = i_func(slu[c1], slu[c2], x_arr[i], y_arr[i])
-  if t < 0:
-    print('ERROR!')
-    break
-  else:
-    res += t
+    if request.method == 'POST':
+        f = request.form['csvfile']
+        x_arr, y_arr = np.array(list()), np.array(list())
+        x_arr = []
+        y_arr = []
+        with open(f, 'r') as file:
+            csvfile = csv.reader(file)
+            for line in csvfile:
+                t = ''.join(line).split(';')
+                x_arr = np.append(x_arr, t[0])
+                y_arr = np.append(y_arr, t[1])
 
-#show the graphics
-plot(r_func(slu['c1'], slu['c2']))
+                x_arr_int = x_arr.astype('f')
+                y_arr_int=y_arr.astype('f')
 
+            data_graph = pd.DataFrame({"X": x_arr_int, "Y": y_arr_int})
+
+            sns.lineplot(x="X", y="Y", data=data_graph)
+
+
+            return render_template('data.html')
+
+
+
+
+@app.route('/visualize')
+def visualize():
+    canvas = FigureCanvas(fig)
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype='img/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
